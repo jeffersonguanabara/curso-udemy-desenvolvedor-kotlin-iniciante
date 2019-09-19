84519929569a2e8c9851d21fe5638ea8760779e9
jeffersonguanabara <jefferson.guanabara@dcx.ufpb.br>
2019-09-19 16:22:21 -0300

    estudando manipulacao de strings

--- src/main.kt
+++ src/main.kt
@@ -1,14 +1,15 @@
 fun main() {
-    // variaveis mutaveis
-    var name = "Pedro Paulo"
-    // variaveis mutaveis são aquelas que pode ter seu valor trocado a qualquer momento
-    name = "Joana D'arc"
-    // variaveis imutaveis
-    val idade = 23
-    // variaveis imutaveis pode ser declarada sem a atribuição de um valor
-    // mas ápos o valor da variavel ser definido ele não pode mais ser trocado.
-    val x
-    x = 10
 
-    println(name)
+    val frase = "Kotlin é uma linguagem"
+    val caracteristica = " show!"
+
+    // concatenando strings
+    print("Kotlin é uma linguagem$caracteristica")
+
+    val str = """sjfadsfjhgasad
+        |jahslkjdahslkjd
+        |kjahslkdjhasljkd
+        |    alksjlkjasldja
+    """.trimMargin()
+    println(str)
 }

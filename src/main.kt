85022df582e0467310ccf7eafbf30b3ba78f7828
jeffersonguanabara <jefferson.guanabara@dcx.ufpb.br>
2019-09-28 15:51:47 -0300

    trabalhando com operações matemáticas em kotlin

--- src/main.kt
+++ src/main.kt
@@ -1,15 +1,27 @@
-fun calculaBonus(a : Int, b : Int, c : Int) = "O bônus é: ${a + b * c * 2}"
 
-fun hello(nome : String) : String = "Olá, $nome"
-
-fun soma(a : Int, b : Int) = a + b
 
 fun main() {
-    val a = 10
-    val b = 20
-    val c = 30
 
-    println(calculaBonus(a, b, c))
-    println(soma(a, b))
-    println(hello("Pedro"))
+    // Operações matemáticas
+    println("2 + 2 = ${2 + 2}")
+    println("3 - 2 = ${3 - 2}")
+    println("4 x 2 = ${4 * 2}")
+    println("9 / 3 = ${9 / 3}")
+    println("10 % 3 = ${10 % 3}")
+
+    // pré e pos incremento
+    var numero = 10
+    println("numero++: ${numero++}")
+    println("numero--: ${numero--}")
+    println("++numero: ${++numero}")
+    println("--numero: ${--numero}")
+
+    numero+=2
+    println("numero+=2 : ${numero}")
+    numero-=4
+    println("numero-=4 : ${numero}")
+    numero*=5
+    println("numero*=5 : ${numero}")
+    numero/=3
+    println("numero/=3 : ${numero}")
 }

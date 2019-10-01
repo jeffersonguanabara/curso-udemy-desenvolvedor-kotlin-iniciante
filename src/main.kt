e9a8f42799f6a6bdea454fd23178e8f80d530fed
jeffersonguanabara <jefferson.guanabara@dcx.ufpb.br>
2019-10-01 09:34:14 -0300

    resolução da questao 1

--- src/main.kt
+++ src/main.kt
@@ -1,27 +1,10 @@
-
+fun contaCaracteres(palavra: kotlin.String) : Int {
+    return palavra.length;
+}
 
 fun main() {
 
-    // Operações matemáticas
-    println("2 + 2 = ${2 + 2}")
-    println("3 - 2 = ${3 - 2}")
-    println("4 x 2 = ${4 * 2}")
-    println("9 / 3 = ${9 / 3}")
-    println("10 % 3 = ${10 % 3}")
+    var palavra: kotlin.String = "Hipopotamo"
 
-    // pré e pos incremento
-    var numero = 10
-    println("numero++: ${numero++}")
-    println("numero--: ${numero--}")
-    println("++numero: ${++numero}")
-    println("--numero: ${--numero}")
-
-    numero+=2
-    println("numero+=2 : ${numero}")
-    numero-=4
-    println("numero-=4 : ${numero}")
-    numero*=5
-    println("numero*=5 : ${numero}")
-    numero/=3
-    println("numero/=3 : ${numero}")
+    println("Tamanho da palavra ${palavra}: " + contaCaracteres(palavra))
 }
